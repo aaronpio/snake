435e2a081153bad7081b862171886eb69944faf7
Aaron <aaronpiolang@gmail.com>
2019-12-04 18:23:08 +0000

    set up boiler plate for play.js. connects to server

---
+++ play.js
@@ -0,0 +1,19 @@
+const net = require("net");
+
+const connect = () => {
+  const client = net.createConnection({
+    host: "192.168.0.103",
+    port: 50541
+  });
+  // interpret incoming data as text
+  client.setEncoding("utf8");
+
+  client.on("data", data => {
+    console.log(data);
+  });
+
+  return client;
+};
+
+console.log("Connecting ...");
+connect();
